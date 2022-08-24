f28dd51b7c6e0feb802a1746b49fd47e2fe15b1c
MrLeeYH <103474303+MrLeeYH@users.noreply.github.com>
2022-08-24 11:32:43 +0800

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,6 @@
+# write a function that uses merge sort to sort a given list of strings
+# FUNCTION merge(array) mutates array (None return)
+
+# test your function with the following list:
+# list1 = ['501', '0', '2', '42', '9', '404', '13', '169']
+
